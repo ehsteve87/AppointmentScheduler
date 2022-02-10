55e62a040c8f207fe7c80d060048ed87802ed5d3
Steve Decker <sdecker@testout.com>
2022-02-09 18:25:05 -0700

    Added localization to login screen

--- src/main/java/appointment_scheduler/ControllerLoginScreen.java
+++ src/main/java/appointment_scheduler/ControllerLoginScreen.java
@@ -1,4 +1,55 @@
 package appointment_scheduler;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.util.ResourceBundle;
+
 public class ControllerLoginScreen {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Label lblUsername;
+
+    @FXML
+    private Label lblZoneId;
+
+    @FXML
+    private PasswordField pfPassword;
+
+    @FXML
+    private TextField tfUsername;
+
+    public void initialize(){
+        ResourceBundle localizedText = ResourceBundle.getBundle("languageBundle/loginScreen");
+        System.out.println(localizedText);
+        lblTitle.setText(localizedText.getString("title"));
+        lblUsername.setText(localizedText.getString("username"));
+        lblPassword.setText(localizedText.getString("password"));
+        btnLogin.setText(localizedText.getString("login"));
+
+
+    }
+
+    @FXML
+    void loginButton(ActionEvent event) {
+
+    }
+
 }
+
+
+
+
--- src/main/java/appointment_scheduler/Main.java
+++ src/main/java/appointment_scheduler/Main.java
@@ -6,11 +6,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainPage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("loginScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Appointment Scheduler");
         stage.setScene(scene);
@@ -18,6 +20,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("fr"));
         launch();
     }
 }
